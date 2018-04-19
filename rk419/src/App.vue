4c468180710797aa75f616f1f423d09e3eb75d6a
zz6101518 <34826302+zz6101518@users.noreply.github.com>
2018-04-19 10:29:32 +0800

    Add files via upload

---
+++ rk419/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-for="i in data">
+          {{i.id}}---{{i.name}}----{{i.addr}}
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name:"app",
+    data(){
+        return {
+          data:[]
+        }
+    },
+    beforeCreate(){
+        
+        this.$ajax({
+          url:"/api",
+          type:"get"
+        }).then((data)=>{
+          console.log(data.list)
+          this.data=data.list
+        })
+    }
+
+  }
+</script>
